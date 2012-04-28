3d56b088c82b7d7487f50680b4f75d576cacb3b6
Tom Oakes <toaksie@gmail.com>
2012-04-28 22:53:21 +0200

    hope this works

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,36 +1,57 @@
 require 'spec_helper'
+# Run the generator again with the --webrat flag if you want to use webrat methods/matchers
 
 describe "Static pages" do
 
 
   subject { page }
 
-  describe "Home page" do
-    before { visit root_path }
+  shared_examples_for "all static pages" do
+    it { should have_selector('h1', text: heading) }
+    it { should have_selector('title', text: full_title(page_title)) }
+  end
 
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      it { should have_selector('h1', text: 'CrowdRenta')  }
-      it { should have_selector('title', text: full_title('')) }
+  describe "Home page" do
+      before { visit root_path }
+      let(:heading) { 'CrowdRenta' }
+      let(:page_title) { '' }
+      it_should_behave_like "all static pages"
       it { should_not have_selector 'title', text: '| Home' }
     end
 
   describe "Help page" do
-    before { visit help_path }
-
-      it { should have_selector('h1', text: 'Help') }
-      it { should have_selector('title', text: full_title('Help')) }
+      before { visit help_path }
+      let(:heading) { 'Help' }
+      let(:page_title) { 'Help' }
+      it_should_behave_like "all static pages"
    end
 
   describe "About page" do
-    before { visit about_path }
-      it { should have_selector('h1', text: 'About Us') }
-      it { should have_selector('title', text: full_title('About Us')) }
+      before { visit about_path }
+      let(:heading) { 'About Us' }
+      let(:page_title) { 'About Us' }
     end
 
 
   describe "Contact page" do
     before { visit contact_path }
-      it { should have_selector('h1', text: 'Contact Us') }
-      it { should have_selector('title', text: full_title('Contact Us')) }
+    let(:heading) { 'Contact Us' }
+    let(:page_title) { 'Contact Us' }
+    it_should_behave_like "all static pages"
+  end
+
+  it "should have the right links on the layout" do
+    visit root_path
+    click_link "About"
+    page.should have_selector 'title', text: full_title('About Us')
+    click_link "Help"
+    page.should have_selector 'title', text: full_title('Help')
+    click_link "Contact"
+    page.should have_selector 'title', text: full_title('Contact Us')
+    click_link "Home"
+    page.should have_selector 'title', text: full_title('')
+    click_link "Sign up now!"
+    page.should have_selector 'title', text: full_title('Signup')
   end
 end
+
