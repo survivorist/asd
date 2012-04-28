69240603958d7c147e4de9c5096f54fe4fcf10ad
Tom Oakes <toaksie@gmail.com>
2012-04-28 22:48:47 +0200

    Rspec and Styling understood

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -5,8 +5,8 @@ describe "UserPages" do
   subject { page }
   describe "user_pages" do
     before { visit signup_path}
-    it  { should have_selector('h1', text: 'Sign Up') }
-    it  { should have_selector('title', text: full_title('Sign Up')) }
+    it  { should have_selector('h1', text: 'Signup') }
+    it  { should have_selector('title', text: full_title('Signup')) }
       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
     end
   end
