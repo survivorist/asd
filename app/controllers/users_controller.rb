0a786c6e30b03429f7fb8cff7e9568f09b1c6490
Tom Oakes <toaksie@gmail.com>
2012-05-03 16:19:51 +0200

    Finish user sign-up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(params[:user])
       if @user.save
-        # Handle a successful save
+        flash[:success] = "Welcome to CrowdRenta!"
+        redirect_to @user
+
       else
         render 'new'
       end
